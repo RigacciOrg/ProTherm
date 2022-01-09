a0dc9c22e7a6f4b0b2b3d716166b501708a4ad12
Niccolo Rigacci <niccolo@rigacci.org>
2022-01-09 18:36:36 +0100

    Send signal to ProTherm process via sysvinit/PID or via systemd service

--- html/changemode.php
+++ html/changemode.php
@@ -1,3 +1,9 @@
 <?php
 
-system('sudo /etc/init.d/protherm change_mode', $ret);
+if (file_exists('/etc/init.d/protherm')) {
+    // Use sysvinit start/stop script to send SIGUSR1 signal.
+    system('sudo /etc/init.d/protherm change_mode', $ret);
+} else {
+    // Uses systemd to send SIGUSR1 signal.
+    system('sudo /usr/local/sbin/protherm-sigusr1', $ret);
+}
